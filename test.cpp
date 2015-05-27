001a2f1ef357721ec93c4ba4df972486448f218c
Isac Arnekvist <isacar@kth.se>
2015-05-27 09:51:32 +0200

    some tests

--- test.cpp
+++ test.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 using namespace std;
 
+struct point {
+    int x;
+    int y;
+};
+
 int main() {
-    vector<int> test;
-    int i;
-    for(i = 0; i < 10; i++) {
-        test.push_back(i);
-    }
-    for(i = 0; i < 10; i++) {
-        cout << test[i] << endl;
-    }
-    cout << test[-2];
+    queue<point> q;
+    point p1;
+    p1.x = 1;
+    p1.y = 2;
+    point p2;
+    p2.x = 3;
+    p2.y = 4;
+    q.push(p1);
+    q.push(p2);
+    p1.x = 666;
+    point p3 = q.front();
+    q.pop();
+    q.pop();
+    cout << p3.x << "," << p3.y << endl;;
     return 0;
 }
