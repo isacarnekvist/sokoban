9f3af884df346cdead84afa6c6041ed13faeefd1
Isac Arnekvist <isacar@csc.kth.se>
2015-05-27 10:15:52 +0200

    ubuntu warnings fixed

--- bpt.cpp
+++ bpt.cpp
@@ -4,6 +4,8 @@
 #include <queue>
 #include <assert.h>
 #include <stack>
+#include <stdlib.h>
+#include <string.h>
 
 using namespace std;
 
@@ -13,7 +15,7 @@ struct point {
 };
 
 // Checks both out of 
-bool isvalidmove(int x, int y, vector<string> board) {
+bool isvalidmove(unsigned int x, unsigned int y, vector<string> board) {
     if (x < 0 || y < 0)         return false;
     if (y >= board.size())      return false;
     if (x >= board[y].size())   return false;
@@ -24,6 +26,8 @@ bool isvalidmove(int x, int y, vector<string> board) {
     if (board[y][x] == ' ')     return true;
     cerr << "Case in 'isvalidmove()' not covered: " << (int)board[y][x] << endl;
     exit(-1);
+    // For compiler happiness
+    return false;
 }
 
 void tests(void);
@@ -36,8 +40,8 @@ int main(int argc, char **argv)
         cout << "Tests OK" << endl;
     }
 
-    int xmax = 0; 
-    int ymax = 0;
+    unsigned int xmax = 0; 
+    unsigned int ymax = 0;
     // Read the board
     std::vector<std::string> board;
     for (std::string line; std::getline(std::cin, line);) {
@@ -45,29 +49,31 @@ int main(int argc, char **argv)
         board.push_back(line);
     }
     ymax = board.size();
+    cout << ymax << endl;
 
     // Create matrix that hold all 'turns'
+    // TODO: Some kind of bug here
     char** routemap = new char*[ymax];
-    for(int i=0; i<xmax; i++) {
+    for(unsigned int i=0; i<xmax; i++) {
         routemap[i] = new char[xmax];
     }
 
-    // Search for starting position
-    point startpos;
-    startpos.x = -1;
-    for (int y = 0; y < board.size(); y++) {
-        for (int x = 0; x < board[y].size(); x++) {
-            if (board[y][x] == '@' || board[y][x] == '+') {
-                startpos.x = x;
-                startpos.y = y;
-            }
-        }
-    }
-    if(-1 == startpos.x) {
-        // No player found
-        cout << "no path" << endl;
-        exit(0);
-    }
+    //// Search for starting position
+    //point startpos;
+    //startpos.x = -1;
+    //for (unsigned int y = 0; y < board.size(); y++) {
+    //    for (unsigned int x = 0; x < board[y].size(); x++) {
+    //        if (board[y][x] == '@' || board[y][x] == '+') {
+    //            startpos.x = x;
+    //            startpos.y = y;
+    //        }
+    //    }
+    //}
+    //if(-1 == startpos.x) {
+    //    // No player found
+    //    cout << "no path" << endl;
+    //    //exit(0);
+    //}
 
     // BFS to find shortest route
     // put root in an empty queue
@@ -76,76 +82,77 @@ int main(int argc, char **argv)
     //     for all neighbours
     //         put at end of queue
     // endwhile
-    queue<point> q;
-    // put root in an empty queue
-    q.push(startpos);
-    point pos;
-    point goal; goal.x = -1; goal.y = -1;
-    char postype;
+    //queue<point> q;
+    //// put root in an empty queue
+    //q.push(startpos);
+    //point pos;
+    //point goal; goal.x = -1; goal.y = -1;
+    //char postype;
 
-    while(!q.empty()) {
-        // pop first node in queue
-        pos = q.front();
-        q.pop();
+    //while(!q.empty()) {
+    //    // pop first node in queue
+    //    pos = q.front();
+    //    assert(q.size() > 0);
+    //    q.pop();
 
-        postype = board[pos.y][pos.x];
+    //    postype = board[pos.y][pos.x];
 
-        // Check if goal
-        if(postype == '.' || postype == '+') {
-            goal = pos;
-            break;
-        }
+    //    // Check if goal
+    //    if(postype == '.' || postype == '+') {
+    //        goal = pos;
+    //        break;
+    //    }
 
-        // Place wall where visited
-        board[pos.y][pos.x] = '#';
+    //    // Place wall where visited
+    //    board[pos.y][pos.x] = '#';
 
-        // Check where to go from here
-        point next;
-        next.x = pos.x-1; next.y = pos.y;
-        if(isvalidmove(next.x, next.y, board)) {
-            q.push(next); routemap[next.y][next.x] = 'L';
-        }
-        next.x = pos.x+1; next.y = pos.y;
-        if(isvalidmove(next.x, next.y, board)) {
-            q.push(next); routemap[next.y][next.x] = 'R';
-        }
-        next.x = pos.x; next.y = pos.y-1;
-        if(isvalidmove(next.x, next.y, board)) {
-            q.push(next); routemap[next.y][next.x] = 'U';
-        }
-        next.x = pos.x; next.y = pos.y+1;
-        if(isvalidmove(next.x, next.y, board)) {
-            q.push(next); routemap[next.y][next.x] = 'D';
-        }
-    }
-    
-    stack<char> route;
-    if(goal.x != -1) {
-        while(pos.x != startpos.x || pos.y != startpos.y) {
-            route.push(routemap[pos.y][pos.x]);
-            switch (routemap[pos.y][pos.x]) {
-                case 'U':
-                    pos.y++;
-                    break;
-                case 'D':
-                    pos.y--;
-                    break;
-                case 'L':
-                    pos.x++;
-                    break;
-                case 'R':
-                    pos.x--;
-                    break;
-            }
-        }
-        while(!route.empty()) {
-            cout << route.top();
-            route.pop();
-        }
-        cout << endl;
-    } else {
-        cout << "no path" << endl;
-    }
+    //    // Check where to go from here
+    //    point next;
+    //    next.x = pos.x-1; next.y = pos.y;
+    //    if(isvalidmove(next.x, next.y, board)) {
+    //        q.push(next); routemap[next.y][next.x] = 'L';
+    //    }
+    //    next.x = pos.x+1; next.y = pos.y;
+    //    if(isvalidmove(next.x, next.y, board)) {
+    //        q.push(next); routemap[next.y][next.x] = 'R';
+    //    }
+    //    next.x = pos.x; next.y = pos.y-1;
+    //    if(isvalidmove(next.x, next.y, board)) {
+    //        q.push(next); routemap[next.y][next.x] = 'U';
+    //    }
+    //    next.x = pos.x; next.y = pos.y+1;
+    //    if(isvalidmove(next.x, next.y, board)) {
+    //        q.push(next); routemap[next.y][next.x] = 'D';
+    //    }
+    //}
+
+    //stack<char> route;
+    //if(goal.x != -1) {
+    //    while(pos.x != startpos.x || pos.y != startpos.y) {
+    //        route.push(routemap[pos.y][pos.x]);
+    //        switch (routemap[pos.y][pos.x]) {
+    //            case 'U':
+    //                pos.y++;
+    //                break;
+    //            case 'D':
+    //                pos.y--;
+    //                break;
+    //            case 'L':
+    //                pos.x++;
+    //                break;
+    //            case 'R':
+    //                pos.x--;
+    //                break;
+    //        }
+    //    }
+    //    while(!route.empty()) {
+    //        cout << route.top();
+    //        route.pop();
+    //    }
+    //    cout << endl;
+    //} else {
+    //    cout << "no path" << endl;
+    //}
 
     return 0;
 }
